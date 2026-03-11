d58860874566304b9ef6520aeddb869bd9116a4f
Kennedy Gharabi <h24kengh@du.se>
2026-03-11 19:06:06 +0100

    Lektion 1 - Uppsättning av miljö

---
+++ server/models/post.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("product", {
+        id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+        }
+    });
+};
