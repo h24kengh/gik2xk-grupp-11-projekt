86cc815f6041ba8d1cef0acb640d1a972ff4f8cd
Elvis Österlund <h24elvos@du.se>
2026-03-16 21:04:12 +0100

    Update postService.js

--- Services/postService.js
+++ Services/postService.js
@@ -50,17 +50,49 @@ async function create(post) {
     const invalidData = validate(post, constraints);
     if (invalidData) {
        return createResponseError(422, invalidData);
-    } else {
-        try {
+    }  try {
            const newPost =  await db.post.create(post);
            return createResponseSuccess(newPost);
         } catch (error) {
         return createResponseError(error.status, error.message);
     }
-  }
+  
 }
-function update() {}
-function destroy() {}
+async function update(post, id) {
+        const invalidData = validate(post, constraints);
+        if(!id) {
+            return createResponseError(422, "ID är obligatoriskt");
+        }
+        if(invalidData) {
+           return createResponseError(422, invalidData);
+        } 
+            try {
+            await db.post.update(post, {
+                where: { id }
+            });
+            return createResponseMessage(200, "Inlägget uppdaterades.");
+            } catch(error) {
+            return createResponseError(error.status, error.message);
+            }
+           
+        
+}
+async function destroy(id) {
+    if(!id) {
+        return createResponseError(422, "ID är obligatoriskt!");
+    }
+    try {
+    await db.post.destroy({
+        where: { id }
+      });
+      return createResponseMessage(200, "Inlägget raderades.");
+    } catch(error) {
+            return createResponseError(error.status, error.message);
+    }
+}
 
 module.exports = { 
-    getAll, create, update, destroy };
+    getAll, 
+    create, 
+    update, 
+    destroy };
