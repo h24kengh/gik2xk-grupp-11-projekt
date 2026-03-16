1aa2ac3aa860ebac138f35033e2d43d34fdc623d
Elvis Österlund <h24elvos@du.se>
2026-03-16 20:20:54 +0100

    Lektion 3

---
+++ Services/postService.js
@@ -0,0 +1,66 @@
+const db = require ('../models');
+
+const {
+    createResponseSuccess,
+    createResponseError,
+    createResponseMessage
+} = require('../Helpers/responsehelper');
+const validate = require('validate.js');    
+const { post } = require('../server/app');
+
+const constraints = { 
+    email: { 
+        length: {
+        minimun: 4,
+        maximum: 200,
+        tooShort: "^E-postadressen måste vara minst %{count} tecken lång.",
+        tooLong: "^E-postadressen får inte vara längre än %{count} tecken lång."
+        },
+        email: {
+        message: "^E-postadressen måste vara en giltig e-postadress."
+        }
+},
+username: {
+length: {
+        minimun: 3,
+        maximum: 50,
+        tooShort: "^Användarnamnet måste vara minst %{count} tecken långt.",
+        tooLong: "^Användarnamnet får inte vara längre än %{count} tecken långt."
+        }
+},
+imageurl: {
+    url: {
+        message: 'Sökvägen är felaktig.'
+    }
+}
+};
+
+async function getAll() {
+    try {
+
+        const allPosts =  await db.post.findAll()
+        /* Om allt blev bra, returna allPosts */
+        return createResponseSuccess(allPosts);
+    } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
+ 
+}
+async function create() {
+    const invalidData = validate(post, constraints);
+    if (invalidData) {
+       return createResponseError(422, invaldidData);
+    } else {
+        try {
+           const newPost =  await db.post.create(post);
+           return createResponseSuccess(newPost);
+        } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
+  }
+}
+function update() {}
+function destroy() {}
+
+module.exports = { 
+    getAll, create, update, destroy };
