51ac94ce818d62599d7eb1a606ea99efc0db80ce
Elvis Österlund <h24elvos@du.se>
2026-03-16 23:15:59 +0100

    Update tagsRoute.js

--- server/routes/tagsRoute.js
+++ server/routes/tagsRoute.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
 const db = require("../models");
+const postService = require("../services/postService")
+
+router.get("/:id/posts", (req, res) => {
+   const id = req.params.id;
+  
+      postService.getByTag(id).then ((result) => {
+      res.status(result.status).json(result.data);
+     });
+
+});
 
 router.get('/', (req, res) => {
     db.tag.findAll().then((result) => {
         res.send(result);
     });
 });
-
+ 
 router.post('/', (req, res) => {
     const tag = req.body;
          db.tag.create(tag).then((result) => {
