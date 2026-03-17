a225889a43414927ab61ac4d9733fab6e05b035e
Kennedy Gharabi <h24kengh@du.se>
2026-03-17 13:21:57 +0100

    Lektion 4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Products from './views/Products.jsx';
+import ProductDetail from './views/ProductDetail.jsx';
+import Cart from './views/Cart.jsx';
+import ProductForm from './views/ProductForm.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, 
+    children: [
+  {path: '/', element: <Products />},
+  {path: '/product/:id', element: <ProductDetail/>},
+  {path: '/cart', element: <Cart/>}, 
+  {path: '/product/:id/edit', element: <ProductForm />},
+  {path: '/product/new', element: <ProductForm />}
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ client/src/views/ProductForm.jsx
@@ -0,0 +1,5 @@
+function ProductForm() {
+    return ( <h2>ProductForm</h2> );
+}
+
+export default ProductForm;
