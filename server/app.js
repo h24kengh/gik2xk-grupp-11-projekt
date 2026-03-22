3c9f5230794d85ee15b77e0e3cbf8c0dd4d1dcb8
Elvis Österlund <h24elvos@du.se>
2026-03-22 12:41:56 +0100

    Lektion 6

--- server/app.js
+++ server/app.js
@@ -8,6 +8,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use ((req, res, next) => {
+    res.header ('Access-Control-Allow-Origin', '*');
+    res.header ('Access-Control-Allow-Headers', '*');
+    res.header ('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+    next();
+})
+
 app.use('/product', require('./routes/productRoute'));
 app.use('/users', require('./routes/usersRoute'));
 app.use('/tag', require('./routes/tagsRoute'));
